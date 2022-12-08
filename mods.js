759d1d6e48a4754ec445b02ff38c369774343408
Harry Moore <harrymoore@yahoo.com>
2022-12-08 17:21:31 -0500

    updates

---
+++ mods.js
@@ -0,0 +1,23 @@
+define(function(require, exports, module) {
+    var $ = require("jquery");
+
+    $(document).on('cloudcms-ready', function(ev) {
+        // find the "All Journals button"
+
+        try {
+            var url = window.location.href;
+            if (url && -1 !== url.indexOf('/tasks/') ) {
+                // current view is in a task context so update the All Jounals link to use the same context
+                var task = url.substring(url.indexOf('/tasks/')+7);
+                task = task.substring(0, task.indexOf('/'));
+ 
+                var branch = $("a[data-item-key='content/all-journals']").attr('href').substring($("a[data-item-key='content/all-journals']").attr('href').indexOf('/branches/')+10);;
+                branch = branch.substring(0, branch.indexOf('/'));
+                 
+                $("a[data-item-key='content/all-journals']").attr('href', $("a[data-item-key='content/all-journals']").attr('href').replace("/branches/"+branch, "/tasks/"+task));
+                console.log($("a[data-item-key='content/all-journals']").attr('href'));
+            }
+        } finally {}
+    });
+
+});
